9ed67aa72815990fe41489002b1905ee51eb674c
ama1ia <45046116+ama1ia@users.noreply.github.com>
2020-05-17 00:19:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ popups = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12]
 popups = popups 
 
 function popupBox() {
-  popups.classList.toggle("show");
+  popups[0].classList.toggle("show");
 }
 
 function firstImage() {
