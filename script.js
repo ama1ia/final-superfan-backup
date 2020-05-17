30cca09e2e2e8f2a3f3f447a1b6f6aa6e2118b1a
ama1ia <45046116+ama1ia@users.noreply.github.com>
2020-05-16 23:50:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
   var p12 = document.getElementById("TMh");
 
 popups = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12]
-popups = a
+a = popups
 
 function popupBox() {
   popups[a].classList.toggle("show")
