32ef666ce3ab621dfa5e2ccddf4b1c3974343689
ama1ia <45046116+ama1ia@users.noreply.github.com>
2020-05-17 18:07:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
   var p11 = document.getElementById("MP");
   var p12 = document.getElementById("TMh");
 
-popups = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12]
+var popups = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12]
 popups = popups 
 
 function popupBox() {
